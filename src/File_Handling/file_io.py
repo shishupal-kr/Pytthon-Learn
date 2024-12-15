baa91952676ace5ab0795fe5b76387a435c0034f
Shishupal <shishupalking@gmail.com>
2024-12-15 11:25:51 +0530

    added file handling package

--- src/File_Handling/file_io.py
+++ src/File_Handling/file_io.py
@@ -7,15 +7,44 @@ Character  Meaning
 't'        text mode (default)
 +          open a disk file for updating (reading and writing)
 'b'        binary mode
+ etc....
+
     open("file.txt","mode")
 data = f.read() read all data
 data = f.readline() read one line
 """
 
+
+# there is two ways of writing
 f = open("spec.txt", "r")
 data = f.read()
-
 print(data)
-print(type(data))
-print(len(data))
 f.close()
+
+#or
+
+with open("spec.txt", "r") as f :  #with always close the f
+   data =  f.read()
+   print(data)
+
+
+f = open("list.txt","w")
+data = f.write("hey shishupal")
+print(data)
+
+f = open("list.txt", "a+")
+data = f.write("\nadding some line in last of hey shishupal")
+print(data)
+
+f = open("list.txt", "a+")
+data = f.write("\nRukkhi is here where are you")
+print(data)
+
+f = open("list.txt", "r")
+data = f.read()
+
+ne=data.replace("shishupal","sh")
+print(ne)
+
+
+
--- src/File_Handling/write.py
+++ src/File_Handling/write.py
@@ -1,5 +1,4 @@
-f = open("spec.txt", "w") # adding some text to file
-data = f.write("hey i have edited this data through python file handling\n")
-
-f = open("spec.txt", "a") # adding some text at end of line
-data = f.write("i am learning python file handling")
+with open("spec.txt", "r") as f:
+    data = f.read()
+ne= data.replace("python","java")
+print(ne)
