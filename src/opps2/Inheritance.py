dc8e055a363fad10aa68e5dd34362589d47cb466
Shishupal <shishupalking@gmail.com>
2024-12-16 12:06:33 +0530

    code added in inheritance

--- src/opps2/Inheritance.py
+++ src/opps2/Inheritance.py
@@ -1,1 +1,24 @@
-created inheritance file
+"""
+It allows one class (child class) to inherit properties and methods from another class (parent class).
+This promotes code reusability, polymorphism, and modularity.
+"""
+# Parent classes
+class Father:
+    def skill(self):
+        return "Driving"
+
+class Mother:
+    def skill(self):
+        return "Cooking"
+
+# Child class (inherits from Father and Mother)
+class Child(Father, Mother):
+    def own_skill(self):
+        return "Coding"
+
+#object of child class
+child = Child()
+
+# Access skills from both parent classes and child class
+print("Father's skill:", child.skill())
+print("Child's own skill:", child.own_skill())
