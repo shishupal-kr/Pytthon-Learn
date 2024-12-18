4a1f798ae79f22bf23c732e7da5ff6c51fdb6fb1
Shishupal <shishupalking@gmail.com>
2024-12-18 19:41:33 +0530

    finalize static method file

--- src/opps1/static_method.py
+++ src/opps1/static_method.py
@@ -1,11 +1,26 @@
-class mac:
-    @staticmethod
-    def model():
-        return "Macbook Air"
-    @staticmethod
-    def company():
-        return "Apple"
-#static method/function is class level method/function
+class Bike:
+    company = "Bajaj"
+    model = "Pulsar 150"
 
-print(mac.model())
-print(mac.company())
+    @staticmethod
+    def start():
+        return "Bike has started"
+    @staticmethod
+    def stop():
+        return "Bike has stopped"
+    @staticmethod
+    def self ():
+        return "Bike ready to go"
+
+#static method/function is class level method/function
+b1 = Bike()
+
+print(b1.start())
+print(b1.stop())
+
+print(Bike.company)
+print(Bike.model)
+
+b2 = Bike()
+print(b2.self())
+print(b2.self)  #location of self method in memory
