734a4ebee7e868e4fd91e9a9ff19d9e199038a11
Shishupal <shishupalking@gmail.com>
2025-08-10 11:41:05 +0530

    added sorting

---
+++ src/numpy/sort.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([3,2,0,1,7,4,6,5])
+print(np.sort(arr))
+
+arr2 = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr2))
+
+arr3 = np.array([True,False,True])
+print(np.sort(arr3))
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+print(np.sort(arr))
